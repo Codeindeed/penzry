71e526762af9b110887091a513297878d13a3bfe
Jeremiah Oparaugo <jerryoparaugo@gmail.com>
2023-10-14 15:24:49 +0100

    Create Offer Screen Logic and Mobile responsiveness 3

--- src/features/feedback/components/feedback-old/Index.tsx
+++ src/features/feedback/components/feedback-old/Index.tsx
@@ -2,7 +2,6 @@ import React, { useState, ChangeEvent } from "react";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import Button from "../../../../Components/ui/Button";
 import { AudioRecorder } from "react-audio-voice-recorder";
-import Marks from "../../../../Components/progress-range/Index";
 
 interface FormField {
   id: number;
@@ -31,6 +30,8 @@ const FeedbackForm: React.FC = () => {
   });
 
   const [onEdit, setOnEdit] = useState<boolean>(false);
+  const [textField, setTextField] = useState<string>("");
+  const [textField2, setTextField2] = useState<string>("");
   const [editedField, setEditedField] = useState<string>("");
 
   const publishFeedback = () => {
@@ -60,6 +61,40 @@ const FeedbackForm: React.FC = () => {
     if (fieldIndex > -1) {
       formFields[fieldIndex].label = fieldLabel;
       setFormContent(formFields);
+    }
+  };
+
+  const editFieldType = (fieldName: string, fieldType: string) => {
+    const validFieldTypes: (
+      | "short_answer"
+      | "paragraph"
+      | "multichoice"
+      | "multiCheckbox"
+    )[] = ["short_answer", "paragraph", "multichoice", "multiCheckbox"];
+
+    if (validFieldTypes.includes(fieldType as any)) {
+      // Use type assertion as any
+      const formFields: FormField[] = [...formContent];
+      const fieldIndex = formFields.findIndex((f) => f.name === fieldName);
+      if (fieldIndex > -1) {
+        formFields[fieldIndex].question_type = fieldType as
+          | "short_answer"
+          | "paragraph"
+          | "multichoice"
+          | "multiCheckbox"; // Use type assertion
+        setFormContent(formFields);
+      }
+    }
+  };
+
+  const addFieldOption = (fieldName: string, option: string) => {
+    const formFields: FormField[] = [...formContent];
+    const fieldIndex = formFields.findIndex((f) => f.name === fieldName);
+    if (fieldIndex > -1) {
+      if (option && option !== "") {
+        formFields[fieldIndex].list.push(option);
+        setFormContent(formFields);
+      }
     }
   };
 
@@ -137,7 +172,7 @@ const FeedbackForm: React.FC = () => {
                 <div className="flex justify-between items-start md:items-center flex-col md:flex-row space-y-2">
                   <div
                     key={field.name}
-                    className="block text-sm font-medium cursor-pointer text-gray-700 capitalize px-1 md:px-5 mt-3"
+                    className="block text-sm font-medium text-gray-700 capitalize px-1 md:px-5 mt-3"
                   >
                     {onEdit && editedField === field.name ? (
                       <input
@@ -167,18 +202,111 @@ const FeedbackForm: React.FC = () => {
                       </label>
                     )}
                   </div>
+                  <div className="ml-2.5 md:ml-0">
+                    <select
+                      className="mr-5 focus:ring-1 focus:ring-grey-20 rounded focus:outline-none hover:border-[#0057FF1A] transition-all duration-700 border-grey-20 focus:border-primary"
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                        editFieldType(field.name, e.target.value)
+                      }
+                    >
+                      <option value="short_answer">Short Answer</option>
+                      <option value="paragraph">Paragraph</option>
+                      <option value="multichoice">Multichoice</option>
+                      <option value="multiCheckbox">MultiCheckbox</option>
+                    </select>
+                  </div>
                 </div>
 
                 <div className="my-4 w-full md:px-5 px-1">
                   {field.question_type === "short_answer" && (
-                    <label className="block pl-1 px-5 mx-auto w-[95%] md:ml-2 py-2 md:w-full">
-                      {field.label}
-                    </label>
+                    <input
+                      type="text"
+                      className="px-5 pl-1 mx-auto w-[95%] md:ml-2.5 shadow-sm py-2 block md:w-full focus:ring-1 focus:ring-grey-20 rounded focus:outline-none hover:border-[#0057FF1A] transition-all duration-700 border-grey-20 focus:border-primary"
+                      placeholder={field.label}
+                      readOnly
+                    />
+                  )}
+                  {field.question_type === "paragraph" && (
+                    <textarea
+                      rows={1}
+                      className="px-5 pl-1 mx-auto w-[95%] md:ml-2.5 shadow-sm py-2 block md:w-full focus:ring-1 focus:ring-grey-20 rounded focus:outline-none hover:border-[#0057FF1A] transition-all duration-700 border-grey-20 focus:border-primary"
+                      placeholder={field.label}
+                      readOnly
+                    />
+                  )}
+                  {field.question_type === "multiCheckbox" && (
+                    <div className="flex flex-col space-y-2 -mt-2 md:pl-3.5 px-2">
+                      {field.list.map((option) => {
+                        return (
+                          <div className="mt-2 flex items-center" key={option}>
+                            <input
+                              type="checkbox"
+                              className="peer h-5 w-5 rounded-lg border-[1.5px] border-grey-30 accent-primary transition-all duration-1000 focus:rounded-lg focus:ring-0 focus:ring-primary cursor-pointer"
+                            />
+                            <label
+                              htmlFor={option}
+                              className="ml-3 text-grey-100 peer-checked:text-grey-90 text-body-md"
+                            >
+                              {option}
+                            </label>
+                          </div>
+                        );
+                      })}
+
+                      <div className="flex space-between mt-4">
+                        <input
+                          type="text"
+                          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setTextField2(e.target.value)
+                          }
+                          value={textField2}
+                          placeholder="Add an option"
+                          className="flex-1 py-2 px-4 border-[1.5px] w-full text-body-sm text-grey-90 placeholder:text-grey-30 focus:ring-1 focus:ring-grey-20 focus:outline-none hover:border-[#0057FF1A] transition-all duration-700 border-grey-20 focus:border-primary"
+                        />
+                        <button
+                          className="bg-primary block hover:bg-indigo-900 text-white px-4 -ml-1"
+                          onClick={() => {
+                            addFieldOption(field.name, textField2);
+                            setTextField2("");
+                          }}
+                        >
+                          Add
+                        </button>
+                      </div>
+                    </div>
                   )}
 
-                  <div className="px-5 pl-1 mx-auto w-[95%] md:ml-2 py-2 md:w-full">
-                    <Marks rtl={false} />
-                  </div>
+                  {field.question_type === "multichoice" && (
+                    <div className="my-4 flex flex-col space-y-2 md:pl-3.5 px-2">
+                      <select className="px-5 pl-2.5 shadow-sm py-2 block w-full border focus:ring-1 focus:ring-grey-20 rounded focus:outline-none hover:border-[#0057FF1A] transition-all duration-700 border-grey-20 focus:border-primary">
+                        {field.list.map((item) => (
+                          <option key={item} value={item}>
+                            {item}
+                          </option>
+                        ))}
+                      </select>
+                      <div className="flex space-between mt-2">
+                        <input
+                          type="text"
+                          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setTextField(e.target.value)
+                          }
+                          value={textField}
+                          placeholder="Add an option"
+                          className="flex-1 py-2 px-4 border-[1.5px] w-full text-body-sm text-grey-90 placeholder:text-grey-30 focus:ring-1 focus:ring-grey-20 focus:outline-none hover:border-[#0057FF1A] transition-all duration-700 border-grey-20 focus:border-primary"
+                        />
+                        <button
+                          className="bg-primary block hover:bg-indigo-900 text-white px-4 -ml-1"
+                          onClick={() => {
+                            addFieldOption(field.name, textField);
+                            setTextField("");
+                          }}
+                        >
+                          Add
+                        </button>
+                      </div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
